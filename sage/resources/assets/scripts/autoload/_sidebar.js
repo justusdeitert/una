7a0837bf5f9c8a446b2c38568c8b3ff5078fecac
Justus Deitert <mail@justusdeitert.de>
2019-05-26 08:02:08 +0000

    Add npm packages and plugins

---
+++ sage/resources/assets/scripts/autoload/_sidebar.js
@@ -0,0 +1,75 @@
+// Importing now the ES2015 Version of sidebarjs to work on ie11
+// Bedrock Sage makes trouble with buble converting to es2015
+import {SidebarElement} from 'sidebarjs/lib/umd/sidebarjs';
+
+// if ($('[sidebarjs]').length > 0) {
+//     // Init SidebarJS
+//
+//     const sidebarjs = new SidebarElement({
+//         onOpen: function () {
+//             // console.log('sidebarjs is open');
+//             // $('html').addClass('sidenav-active');
+//             // $('.burger-menu-icon').addClass('icon-close');
+//         },
+//         onClose: function () {
+//             // console.log('sidebarjs is close');
+//             // $('html').removeClass('sidenav-active');
+//             // $('.burger-menu-icon').removeClass('icon-close');
+//             // $('[sidebarjs-backdrop]').prepend( "<p>Test</p>" )
+//         },
+//         onChangeVisibility: function (changes) {
+//             // console.log('sidebarjs is visible?', changes.isVisible);
+//         },
+//         nativeSwipe: false,
+//         position: 'right',
+//     });
+//
+//     const closeSidebar = function() {
+//         sidebarjs.close();
+//         $('body').removeClass('sidenav-active');
+//         $('.sidebar-backdrop').remove();
+//     };
+//
+//     const openSidebar = function() {
+//         sidebarjs.open();
+//         $('body').addClass('sidenav-active');
+//         $('[sidebarjs-backdrop]').before('<div class="sidebar-backdrop"></div>');
+//
+//         $('.sidebar-backdrop').click(function() {
+//             closeSidebar();
+//         });
+//
+//         $('.sidebar-backdrop').swipe({
+//             //Generic swipe handler for all directions
+//             swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
+//                 // console.log("You swiped " + direction );
+//
+//                 if(direction === 'right') {
+//                     closeSidebar();
+//                 }
+//             }
+//         });
+//     };
+//
+//     $('.burger-menu-button').click(function() {
+//         if (sidebarjs.isVisible()) {
+//             closeSidebar();
+//         } else {
+//             openSidebar();
+//         }
+//     });
+//
+//     $('.sidenav-close-icon').click(function() {
+//         closeSidebar();
+//     });
+//
+//     // Close on Resize
+//     $( window ).resize(function() {
+//         closeSidebar();
+//     });
+//
+//     // Close on Resize
+//     $( window ).scroll(function() {
+//         closeSidebar();
+//     });
+// }
---
+++ sage/resources/assets/scripts/autoload/_smart-photo.js
@@ -0,0 +1,9 @@
+// Importing Compiled smartphoto from lib
+import SmartPhoto from 'smartphoto/js/smartphoto.js'
+
+// new SmartPhoto('.smart-photo', {
+//     resizeStyle: 'fit', // resize images to fill/fit on the screen
+//     arrows: true,
+//     nav: false,
+//     useOrientationApi: true
+// });
