01c587f193f7c875705083fedb3a691d8dd68fc0
Justus Deitert <mail@justusdeitert.de>
2019-05-26 10:39:59 +0000

    Add fullpage.js styles and scripts

---
+++ sage/resources/assets/scripts/autoload/_fullpage.js
@@ -0,0 +1,16 @@
+// https://github.com/alvarotrigo/fullPage.js/wiki/Use-module-loaders-for-fullPage.js
+
+// Optional. When using fullPage extensions
+//import scrollHorizontally from './fullpage.scrollHorizontally.min';
+
+// Optional. When using scrollOverflow:true
+//import IScroll from 'fullpage.js/vendors/scrolloverflow';
+
+// Importing fullpage.js
+import fullpage from 'fullpage.js';
+
+// Initializing it
+var fullPageInstance = new fullpage('#fullpage', {
+    navigation: false,
+    // sectionsColor:['#ff5f45', '#0798ec', '#fc6c7c', 'grey']
+});
