c68f5cf708e2c00cd1e8107ab40015d20dc63a15
Justus Deitert <mail@justusdeitert.de>
2019-06-04 10:35:44 +0200

    feat: add plugin styles
    - Adjust head.blade.php
    - Update register-post-types.php

--- sage/resources/includes/register-post-types.php
+++ sage/resources/includes/register-post-types.php
@@ -5,73 +5,73 @@
 add_action('init', function() {
 
     // Add new taxonomy, make it hierarchical (like categories)
-    register_taxonomy('categories', 'doors', array(
-        'labels' => array(
-            'name' => __( 'Categories', 'sage'),
-            'menu_name' => __( 'Categories', 'sage'),
-            'singular_name' => __( 'Category', 'sage'),
-            'search_items' => __( 'Search Categories', 'sage'),
-            'all_items' => __( 'All Categories', 'sage'),
-            // 'parent_item' => __( 'Parent Department', 'sage'),
-            // 'parent_item_colon' => __( 'Parent Fitness Type:', 'sage'),
-            'edit_item' => __( 'Edit Categories', 'sage'),
-            'update_item' => __( 'Update Categories', 'sage'),
-            'add_new_item' => __( 'Add New Category', 'sage'),
-            'new_item_name' => __( 'New Category Name', 'sage'),
-        ),
-        'hierarchical' => true,
-        'public' => true,
-        'show_in_nav_menus' => true,
-        'show_ui' => true,
-        'show_admin_column' => true,
-        'query_var' => true,
-        'rewrite' => array('slug' => 'innentueren', 'with_front' => false),
-    ));
+    // register_taxonomy('categories', 'doors', array(
+    //     'labels' => array(
+    //         'name' => __( 'Categories', 'sage'),
+    //         'menu_name' => __( 'Categories', 'sage'),
+    //         'singular_name' => __( 'Category', 'sage'),
+    //         'search_items' => __( 'Search Categories', 'sage'),
+    //         'all_items' => __( 'All Categories', 'sage'),
+    //         // 'parent_item' => __( 'Parent Department', 'sage'),
+    //         // 'parent_item_colon' => __( 'Parent Fitness Type:', 'sage'),
+    //         'edit_item' => __( 'Edit Categories', 'sage'),
+    //         'update_item' => __( 'Update Categories', 'sage'),
+    //         'add_new_item' => __( 'Add New Category', 'sage'),
+    //         'new_item_name' => __( 'New Category Name', 'sage'),
+    //     ),
+    //     'hierarchical' => true,
+    //     'public' => true,
+    //     'show_in_nav_menus' => true,
+    //     'show_ui' => true,
+    //     'show_admin_column' => true,
+    //     'query_var' => true,
+    //     'rewrite' => array('slug' => 'innentueren', 'with_front' => false),
+    // ));
 
-    register_post_type('Doors', array(
-        'labels' => [
-            'name' => __('Doors', 'sage'),
-            'singular_name'  => __('Door', 'sage'),
-            'menu_name' => __('Doors', 'sage'),
-            'show_in_menu' => __('Doors', 'sage'),
-            'add_new_item' => __('Add New Door', 'sage'),
-        ],
-        'public' => true,
-        'capability_type' => 'post',
-        'has_archive' => 'doors',
-        'show_in_nav_menus' => true,
-        'hierarchical' => false,
-        'supports' => array('title', 'revisions', 'thumbnail'),
-        'menu_icon' => 'dashicons-admin-home',
-        'publicly queryable' => true,
-        // 'taxonomies' => array('categories'),
-        'rewrite' => array( 'slug' => 'innentueren/%categories%', 'with_front' => false)
-        // 'rewrite' => array('slug' => 'innentueren')
-        // 'show_ui'       => true,
-        // 'show_in_menu'  => true,
-    ));
+    // register_post_type('Doors', array(
+    //     'labels' => [
+    //         'name' => __('Doors', 'sage'),
+    //         'singular_name'  => __('Door', 'sage'),
+    //         'menu_name' => __('Doors', 'sage'),
+    //         'show_in_menu' => __('Doors', 'sage'),
+    //         'add_new_item' => __('Add New Door', 'sage'),
+    //     ],
+    //     'public' => true,
+    //     'capability_type' => 'post',
+    //     'has_archive' => 'doors',
+    //     'show_in_nav_menus' => true,
+    //     'hierarchical' => false,
+    //     'supports' => array('title', 'revisions', 'thumbnail'),
+    //     'menu_icon' => 'dashicons-admin-home',
+    //     'publicly queryable' => true,
+    //     // 'taxonomies' => array('categories'),
+    //     'rewrite' => array( 'slug' => 'innentueren/%categories%', 'with_front' => false)
+    //     // 'rewrite' => array('slug' => 'innentueren')
+    //     // 'show_ui'       => true,
+    //     // 'show_in_menu'  => true,
+    // ));
 
-    register_post_type('Fittings', [
-        'labels' => [
-            'name' => __('Fittings', 'sage'),
-            'singular_name'  => __('Fitting', 'sage'),
-            'menu_name' => __('Fittings', 'sage'),
-            'show_in_menu' => __('Fittings', 'sage'),
-            'add_new_item' => __('Add New Fitting', 'sage'),
-        ],
-        'public' => true,
-        'capability_type' => 'post',
-        'has_archive' => false,
-        'show_in_nav_menus' => true,
-        'hierarchical' => false,
-        'supports' => array('title', 'revisions', 'thumbnail'),
-        'menu_icon' => 'dashicons-admin-post',
-        'publicly queryable' => true,
-        'rewrite' => array('slug' => 'fittings')
-        // 'show_ui'       => true,
-        // 'show_in_menu'  => true,
-        // 'taxonomies'         => array('department')
-    ]);
+    // register_post_type('Fittings', [
+    //     'labels' => [
+    //         'name' => __('Fittings', 'sage'),
+    //         'singular_name'  => __('Fitting', 'sage'),
+    //         'menu_name' => __('Fittings', 'sage'),
+    //         'show_in_menu' => __('Fittings', 'sage'),
+    //         'add_new_item' => __('Add New Fitting', 'sage'),
+    //     ],
+    //     'public' => true,
+    //     'capability_type' => 'post',
+    //     'has_archive' => false,
+    //     'show_in_nav_menus' => true,
+    //     'hierarchical' => false,
+    //     'supports' => array('title', 'revisions', 'thumbnail'),
+    //     'menu_icon' => 'dashicons-admin-post',
+    //     'publicly queryable' => true,
+    //     'rewrite' => array('slug' => 'fittings')
+    //     // 'show_ui'       => true,
+    //     // 'show_in_menu'  => true,
+    //     // 'taxonomies'         => array('department')
+    // ]);
 });
 
 
--- sage/resources/views/partials/head.blade.php
+++ sage/resources/views/partials/head.blade.php
@@ -5,6 +5,21 @@
 
     @php wp_head() @endphp
 
+    {{--Implementing Google Icons And Fonts--}}
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    @if(get_field('page_description', 'option'))
+        <meta name="description" content="{{ get_field('page_description', 'option') }}" />
+    @endif
+
+    @if(get_field('favicon', 'option'))
+        <link href="{!! get_field('favicon', 'option')['url'] !!}}" rel="shortcut icon" />
+    @endif
+
+    @if(get_field('touch_icon', 'option'))
+        {{--https://mathiasbynens.be/notes/touch-icons--}}
+        <link href="{!! get_field('touch_icon', 'option')['url'] !!}}" rel="apple-touch-icon-precomposed apple-touch-icon" />
+    @endif
+
 </head>
