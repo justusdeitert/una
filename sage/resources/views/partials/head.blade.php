10fdd6ec6f49c6ae463594a2826ce028418c9d5b
Justus Deitert <mail@justusdeitert.de>
2019-06-04 07:49:34 +0200

    style: add Open Sans font

--- sage/resources/views/partials/head.blade.php
+++ sage/resources/views/partials/head.blade.php
@@ -2,5 +2,8 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
   @php wp_head() @endphp
+
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&display=swap" rel="stylesheet">
 </head>
