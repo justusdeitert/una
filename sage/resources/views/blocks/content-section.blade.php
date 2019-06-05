58929729fdf672b67cf5503c8eb9ec49bf5fd576
Justus Deitert <mail@justusdeitert.de>
2019-06-05 14:29:47 +0200

    feat: add phoenix-media-rename plugin
    - Add phoenix-media-rename plugin
    - Update admin styles

--- sage/resources/views/blocks/content-section.blade.php
+++ sage/resources/views/blocks/content-section.blade.php
@@ -7,36 +7,48 @@
 --}}
 
 @if(get_field('image_section'))
-    <div class="section-desktop image-section">
+    <div class="section-desktop">
         <div class="slide-content">
             @while(has_sub_field('image_section'))
                 @if(get_row_layout() == 'image')
-
-                    @if(get_sub_field('link'))
-                        <a class="admin-prevent-click image-wrapper section-image-{!! get_sub_field('image')['ID'] !!}" href="{!! get_sub_field('link')['url'] !!}">
-                            <div class="hover-overlay">{!! get_sub_field('link')['title'] !!}</div>
-                            <img src="{!! get_sub_field('image')['sizes']['large'] !!}" >
-                        </a>
-                    @else
-                        {{--@php var_dump(get_sub_field('image')['caption']); @endphp--}}
-                        <a data-caption="{!! get_sub_field('image')['caption'] !!}" class="admin-prevent-click image-wrapper smart-photo section-image-{!! get_sub_field('image')['ID'] !!}" href="{!! get_sub_field('image')['url'] !!}"  data-group="desktop-group-{!! get_sub_field('image')['ID'] !!}">
-                            <div class="image-container">
+                    <div class="image-section">
+                        @if(get_sub_field('link'))
+                            <a class="admin-prevent-click image-wrapper section-image-{!! get_sub_field('image')['ID'] !!}" href="{!! get_sub_field('link')['url'] !!}">
+                                <div class="hover-overlay">{!! get_sub_field('link')['title'] !!}</div>
                                 <img src="{!! get_sub_field('image')['sizes']['large'] !!}" >
-                                <div class="caption"><div class="span">{!! get_sub_field('image')['caption'] !!}</div></div>
-                            </div>
-                        </a>
-                    @endif
+                            </a>
+                        @else
+                            {{--@php var_dump(get_sub_field('image')['caption']); @endphp--}}
+                            <a data-caption="{!! get_sub_field('image')['caption'] !!}" class="admin-prevent-click image-wrapper smart-photo section-image-{!! get_sub_field('image')['ID'] !!}" href="{!! get_sub_field('image')['url'] !!}"  data-group="desktop-group-{!! get_sub_field('image')['ID'] !!}">
+                                <div class="image-container">
+                                    <img src="{!! get_sub_field('image')['sizes']['large'] !!}" >
+                                    <div class="caption"><div class="span">{!! get_sub_field('image')['caption'] !!}</div></div>
+                                </div>
+                            </a>
+                        @endif
 
-                    <style>
-                        .section-image-{!! get_sub_field('image')['ID'] !!} {
-                            position: absolute;
-                            @if(get_sub_field('image_width')) width: {!! get_sub_field('image_width') !!}%; @endif
+                        <style>
+                            .section-image-{!! get_sub_field('image')['ID'] !!} {
+                                position: absolute;
+                                @if(get_sub_field('image_width')) width: {!! get_sub_field('image_width') !!}%; @endif
                             @if(get_sub_field('position_top')) top: {!! get_sub_field('position_top') !!}%; @endif
                             @if(get_sub_field('position_right')) right: {!! get_sub_field('position_right') !!}%; @endif
                             @if(get_sub_field('position_bottom')) bottom: {!! get_sub_field('position_bottom') !!}%; @endif
                             @if(get_sub_field('position_left')) left: {!! get_sub_field('position_left') !!}%; @endif
                         }
-                    </style>
+                        </style>
+                    </div>
+                @endif
+                @if(get_row_layout() == 'text')
+                    <div class="text-container">
+                        <div class="row">
+                            <div class="col-12 col-md-10">
+                                @if(get_sub_field('text'))
+                                    <p>{!! get_sub_field('text') !!}</p>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
                 @endif
             @endwhile
         </div>
@@ -45,7 +57,6 @@
     {{--<div class="section-mobile image-section fp-auto-height">--}}
     @while(has_sub_field('image_section'))
         @if(get_row_layout() == 'image')
-
             @if(get_sub_field('link'))
                 <div class="section-mobile {{--fp-auto-height--}}">
                     <div class="section-mobile-inner">
@@ -80,6 +91,19 @@
                             @if(get_sub_field('position_left')) left: {!! get_sub_field('position_left') !!}%; @endif
                         }
             </style>
+        @endif
+        @if(get_row_layout() == 'text')
+            @if(get_sub_field('text'))
+                <div class="section-mobile {{--fp-auto-height--}}">
+                    <div class="text-container">
+                        <div class="row">
+                            <div class="col-12 col-md-10">
+                                <p>{!! get_sub_field('text') !!}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endif
         @endif
     @endwhile
     {{--</div>--}}
