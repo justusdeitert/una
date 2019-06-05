f41db2af650b9efd563955fc2a19fc4df6a1bfca
Justus Deitert <mail@justusdeitert.de>
2019-06-05 15:15:46 +0000

    Update container max-widths
    - Update container max-widths
    - Add big/dark paragraph styles

--- sage/resources/includes/adjust-wp-wysiwyg.php
+++ sage/resources/includes/adjust-wp-wysiwyg.php
@@ -22,13 +22,76 @@ add_filter('tiny_mce_before_init', function($in) {
     return $in;
 });
 
+/**
+ * Change the block formats available in TinyMCE.
+ *
+ * @link http://codex.wordpress.org/TinyMCE_Custom_Styles
+ *
+ * @param array $init Default settings to be overridden.
+ *
+ * @return array The modified $init array.
+ */
+// https://stevegrunwell.com/blog/wordpress-tinymce-block-formats/
+function change_mce_block_formats($init) {
+    $block_formats = array(
+        'Paragraph=p',
+        // 'Heading 1=h1',
+        // 'Heading 2=h2',
+        'Heading 3=h3',
+        // 'Heading 4=h4',
+        // 'Heading 5=h5',
+        // 'Heading 6=h6',
+        // 'Preformatted=pre',
+        // 'Code=code',
+    );
 
-// ----------------------------------->
-// Set TinyMCE editor Formats.
-// ----------------------------------->
-add_filter('tiny_mce_before_init', function($init) {
-    // Add block format elements you want to show in dropdown
-    // $init['block_formats'] = 'Paragraph=p;Heading 2=h2;Heading 3=h3;';
-    $init['block_formats'] = 'Paragraph=p;Heading 3=h3;';
+    $init['block_formats'] = implode( ';', $block_formats );
+
     return $init;
-});
+}
+add_filter( 'tiny_mce_before_init', 'change_mce_block_formats' );
+
+
+// Callback function to filter the MCE settings
+// https://www.wpbeginner.com/wp-tutorials/how-to-add-custom-styles-to-wordpress-visual-editor/
+function my_mce_before_init_insert_formats( $init_array ) {
+
+    // Define the style_formats array
+
+    $style_formats = array(
+        /*
+        * Each array child is a format with it's own settings
+        * Notice that each array has title, block, classes, and wrapper arguments
+        * Title is the label which will be visible in Formats menu
+        * Block defines whether it is a span, div, selector, or inline style
+        * Classes allows you to define CSS classes
+        * Wrapper whether or not to add a new block-level element around any selected elements
+        */
+        // array(
+        //     'title' => 'Content Block',
+        //     'block' => 'span',
+        //     'classes' => 'content-block',
+        //     'wrapper' => true,
+        //
+        // ),
+        array(
+            'title' => 'Dark Text',
+            'block' => 'p',
+            'classes' => 'dark',
+            'wrapper' => false,
+        ),
+        array(
+            'title' => 'Big Text',
+            'block' => 'p',
+            'classes' => 'big',
+            'wrapper' => false,
+        ),
+    );
+    // Insert the array, JSON ENCODED, into 'style_formats'
+    $init_array['style_formats'] = json_encode( $style_formats );
+
+    return $init_array;
+
+}
+// Attach callback to 'tiny_mce_before_init'
+add_filter( 'tiny_mce_before_init', 'my_mce_before_init_insert_formats' );
