a80590d6d9673a1eb436241cf9197b8f18c955f5
Justus Deitert <mail@justusdeitert.de>
2019-06-10 11:43:06 +0200

    Add draggable component

--- sage/resources/includes/register-post-types.php
+++ sage/resources/includes/register-post-types.php
@@ -28,28 +28,28 @@ add_action('init', function() {
     //     'rewrite' => array('slug' => 'innentueren', 'with_front' => false),
     // ));
 
-    // register_post_type('Doors', array(
-    //     'labels' => [
-    //         'name' => __('Doors', 'sage'),
-    //         'singular_name'  => __('Door', 'sage'),
-    //         'menu_name' => __('Doors', 'sage'),
-    //         'show_in_menu' => __('Doors', 'sage'),
-    //         'add_new_item' => __('Add New Door', 'sage'),
-    //     ],
-    //     'public' => true,
-    //     'capability_type' => 'post',
-    //     'has_archive' => 'doors',
-    //     'show_in_nav_menus' => true,
-    //     'hierarchical' => false,
-    //     'supports' => array('title', 'revisions', 'thumbnail'),
-    //     'menu_icon' => 'dashicons-admin-home',
-    //     'publicly queryable' => true,
-    //     // 'taxonomies' => array('categories'),
-    //     'rewrite' => array( 'slug' => 'innentueren/%categories%', 'with_front' => false)
-    //     // 'rewrite' => array('slug' => 'innentueren')
-    //     // 'show_ui'       => true,
-    //     // 'show_in_menu'  => true,
-    // ));
+    register_post_type('Events', array(
+        'labels' => [
+            'name' => __('Events', 'sage'),
+            'singular_name'  => __('Event', 'sage'),
+            'menu_name' => __('Events', 'sage'),
+            'show_in_menu' => __('Events', 'sage'),
+            'add_new_item' => __('Add New Event', 'sage'),
+        ],
+        'public' => true,
+        'capability_type' => 'post',
+        'has_archive' => 'events',
+        'show_in_nav_menus' => true,
+        'hierarchical' => false,
+        'supports' => array('title', 'revisions'/*, 'thumbnail'*/),
+        'menu_icon' => 'dashicons-tag',
+        'publicly queryable' => true,
+        // 'taxonomies' => array('categories'),
+        // 'rewrite' => array( 'slug' => 'innentueren/%categories%', 'with_front' => false)
+        // 'rewrite' => array('slug' => 'innentueren')
+        // 'show_ui'       => true,
+        // 'show_in_menu'  => true,
+    ));
 
     // register_post_type('Fittings', [
     //     'labels' => [
