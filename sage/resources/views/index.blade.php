c07cb7b8e7ac14f16fd5a0e232df90db6a9e1fb1
Justus Deitert <mail@justusdeitert.de>
2019-06-10 17:19:44 +0200

    Add redirects for single, search, 404, and index pages

--- sage/resources/views/index.blade.php
+++ sage/resources/views/index.blade.php
@@ -1,18 +1,25 @@
-@extends('layouts.app')
+{{--@extends('layouts.app')--}}
 
-@section('content')
-  @include('partials.page-header')
+{{--@section('content')--}}
+    {{--@include('partials.page-header')--}}
 
-  @if (!have_posts())
-    <div class="alert alert-warning">
-      {{ __('Sorry, no results were found.', 'sage') }}
-    </div>
-    {!! get_search_form(false) !!}
-  @endif
+    {{--@if (!have_posts())--}}
+        {{--<div class="alert alert-warning">--}}
+            {{--{{ __('Sorry, no results were found.', 'sage') }}--}}
+        {{--</div>--}}
+        {{--{!! get_search_form(false) !!}--}}
+    {{--@endif--}}
 
-  @while (have_posts()) @php the_post() @endphp
-    @include('partials.content-'.get_post_type())
-  @endwhile
+    {{--@while (have_posts())--}}
+        {{--@php the_post() @endphp--}}
+        {{--@include('partials.content-'.get_post_type())--}}
+    {{--@endwhile--}}
 
-  {!! get_the_posts_navigation() !!}
-@endsection
+    {{--{!! get_the_posts_navigation() !!}--}}
+{{--@endsection--}}
+
+@php
+    header("HTTP/1.1 301 Moved Permanently");
+    header("Location: ".get_bloginfo('url'));
+    exit();
+@endphp
--- sage/resources/views/single.blade.php
+++ sage/resources/views/single.blade.php
@@ -1,7 +1,14 @@
-@extends('layouts.app')
+{{--@extends('layouts.app')--}}
 
-@section('content')
-  @while(have_posts()) @php the_post() @endphp
-    @include('partials.content-single-'.get_post_type())
-  @endwhile
-@endsection
+{{--@section('content')--}}
+    {{--@while(have_posts()) --}}
+        {{--@php the_post() @endphp--}}
+        {{--@include('partials.content-single-'.get_post_type())--}}
+    {{--@endwhile--}}
+{{--@endsection--}}
+
+@php
+    header("HTTP/1.1 301 Moved Permanently");
+    header("Location: ".get_bloginfo('url'));
+    exit();
+@endphp
