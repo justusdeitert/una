2d0c6970fc6f9b632e5c032738ab8f694ab47467
Justus Deitert <mail@justusdeitert.de>
2019-06-11 08:48:29 +0000

    Remove metadata handling

--- sage/resources/includes/wp-meta-data.php
+++ sage/resources/includes/wp-meta-data.php
@@ -1,10 +1,13 @@
 <?php
 // https://wordpress.stackexchange.com/questions/91177/save-camera-info-as-metadata-on-image-upload
 // https://codex.wordpress.org/Function_Reference/wp_generate_attachment_metadata
-add_filter( 'wp_generate_attachment_metadata', 'manipulate_metadata_wpse_91177', 10, 2 );
 
-function manipulate_metadata_wpse_91177( $metadata, $attachment_id ) {
-    // var_dump( $metadata['image_meta'] );
-    // Credit is inside $metadata['image_meta']['credit']
-    return $metadata;
-}
+
+// TODO: DOES NOT WORK!!!!
+// add_filter( 'wp_generate_attachment_metadata', 'manipulate_metadata_wpse_91177', 10, 2 );
+//
+// function manipulate_metadata_wpse_91177( $metadata, $attachment_id ) {
+//     // var_dump( $metadata['image_meta'] );
+//     // Credit is inside $metadata['image_meta']['credit']
+//     return $metadata;
+// }
