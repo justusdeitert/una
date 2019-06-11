6fb0bcfbee8345d173e9f31973a464051b0d7b7d
Justus Deitert <mail@justusdeitert.de>
2019-06-11 10:18:38 +0000

    Correct content section template

--- sage/resources/views/blocks/content-section.blade.php
+++ sage/resources/views/blocks/content-section.blade.php
@@ -60,7 +60,7 @@
                     <div class="content-container">
                         <div class="text-container">
                             <div class="row">
-                                <div class="col-12 col-md-11">
+                                <div class="col-12 col-md-10">
                                     <div class="row">
                                         <div class="col-12 col-sm-6">
                                             @if(get_sub_field('first_column'))
