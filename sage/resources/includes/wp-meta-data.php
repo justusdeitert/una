29dadef5bf57b8a4efe93ccbc48ff3eb42f63938
Justus Deitert <mail@justusdeitert.de>
2019-06-11 09:10:34 +0000

    Research WordPress metadata approaches

--- sage/resources/includes/wp-meta-data.php
+++ sage/resources/includes/wp-meta-data.php
@@ -4,10 +4,11 @@
 
 
 // TODO: DOES NOT WORK!!!!
-// add_filter( 'wp_generate_attachment_metadata', 'manipulate_metadata_wpse_91177', 10, 2 );
-//
-// function manipulate_metadata_wpse_91177( $metadata, $attachment_id ) {
-//     // var_dump( $metadata['image_meta'] );
-//     // Credit is inside $metadata['image_meta']['credit']
-//     return $metadata;
-// }
+// define the wp_generate_attachment_metadata callback
+function filter_wp_generate_attachment_metadata( $metadata, $attachment_id ) {
+    // make filter magic happen here...
+    return $metadata;
+};
+
+// add the filter
+add_filter( 'wp_generate_attachment_metadata', 'filter_wp_generate_attachment_metadata', 10, 2 );
