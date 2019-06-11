e3157733f55384f848cdb1494756daac1d8564e6
Justus Deitert <mail@justusdeitert.de>
2019-06-11 08:44:29 +0000

    Update WordPress metadata handling

--- sage/resources/includes/wp-meta-data.php
+++ sage/resources/includes/wp-meta-data.php
@@ -1,4 +1,6 @@
 <?php
+// https://wordpress.stackexchange.com/questions/91177/save-camera-info-as-metadata-on-image-upload
+// https://codex.wordpress.org/Function_Reference/wp_generate_attachment_metadata
 add_filter( 'wp_generate_attachment_metadata', 'manipulate_metadata_wpse_91177', 10, 2 );
 
 function manipulate_metadata_wpse_91177( $metadata, $attachment_id ) {
