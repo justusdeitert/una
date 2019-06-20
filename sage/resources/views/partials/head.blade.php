da3d88e3bbcef34ac6de7d5bfca49b38d4eb779a
Justus Deitert <mail@justusdeitert.de>
2019-06-20 11:49:34 +0000

    feat: add Open Graph images

--- sage/resources/views/partials/head.blade.php
+++ sage/resources/views/partials/head.blade.php
@@ -12,14 +12,9 @@
     {{--Google Site Verification - Confir   m ownership--}}
     <meta name="google-site-verification" content="ciWuq4bh-v2OypfzSbKRdhCO10qDmoRUncd1O1rX6fs" />
 
-    @php wp_head() @endphp
-
-    {{--Implementing Google Icons And Fonts--}}
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
     @if(get_field('page_description', 'option'))
         <meta name="description" content="{{ get_field('page_description', 'option') }}" />
+        <meta property="og:description" content="{{ get_field('page_description', 'option') }}" />
     @endif
 
     @if(get_field('favicon', 'option'))
@@ -29,5 +24,22 @@
     @if(get_field('touch_icon', 'option'))
         {{--https://mathiasbynens.be/notes/touch-icons--}}
         <link href="{!! get_field('touch_icon', 'option')['url'] !!}}" rel="apple-touch-icon-precomposed" />
+        <meta name="msapplication-TileImage" content="{{ get_field('touch_icon', 'option')['url'] }}">
     @endif
+
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="{{ get_bloginfo('name') }}" />
+    <meta property="og:url" content="{{ get_home_url() }}" />
+
+    @if(get_field('open_graph_images', 'option'))
+        @foreach(get_field('open_graph_images', 'option') as $images)
+            <meta property="og:image" content="{{ $images['url'] }}" />
+        @endforeach
+    @endif
+
+    @php wp_head() @endphp
+
+    {{--Implementing Google Icons And Fonts--}}
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
