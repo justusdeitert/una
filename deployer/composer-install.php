4a1d1e5f12f506372b1ae266ec44df5606f1fca6
Justus Deitert <mail@justusdeitert.de>
2019-06-26 07:43:29 +0000

    Adjust Deployer for production

--- deployer/composer-install.php
+++ deployer/composer-install.php
@@ -2,37 +2,12 @@
 
 namespace Deployer;
 
-// Set Binarys for Composer install
-
-// set('bin/php', function () {
-//     return 'php';
-//     // return run('which php');
-// });
-//
-// set('bin/composer', function () {
-//     // return 'composer';
-//     return run('which composer');
-// });
-//
-// // Important for Production
-// set('install_option', 'install --verbose --prefer-dist --no-progress --no-interaction --no-dev --optimize-autoloader');
-//
-// // Adding tasks
-// desc('Installing vendors');
-// task('composer:install', function () {
-//     writeln('Installing Composer for Bedrock: {{release_path}}/bedrock');
-//     run('cd {{release_path}}/bedrock && {{bin/composer}} {{install_option}}');
-//
-//     writeln('Installing Composer for Sage Theme: {{release_path}}/{{theme_path}}');
-//     run('cd {{release_path}}/{{theme_path}} && {{bin/composer}} {{install_option}}');
-// });
-
-namespace Deployer;
-
 set('bin/composer', function () {
     return run('which composer');
 });
 
+// TODO: Define Multiple Array for Composer Paths in the Future!
+// -------------------------------->
 desc('Install Composer packages in Bedrock');
 task('composer:install:bedrock', function () {
     writeln('Install Composer packages in Bedrock');
