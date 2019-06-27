613dedf383d4fa90a6ef82b915f2807b8c9dede1
Justus Deitert <mail@justusdeitert.de>
2019-06-27 08:37:44 +0000

    Remove analytics plugin
    - Remove analytics plugin
    - Update Composer dependencies

--- sage/resources/views/partials/head.blade.php
+++ sage/resources/views/partials/head.blade.php
@@ -30,14 +30,4 @@
         {{--https://mathiasbynens.be/notes/touch-icons--}}
         <link href="{!! get_field('touch_icon', 'option')['url'] !!}}" rel="apple-touch-icon-precomposed apple-touch-icon" />
     @endif
-
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-76171732-7"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'UA-76171732-7');
-    </script>
 </head>
