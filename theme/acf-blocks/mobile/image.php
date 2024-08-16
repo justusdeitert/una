e115907e9222add1fd70e15b41df32262310136a
Justus Deitert <mail@justusdeitert.de>
2024-08-16 17:48:41 +0200

    feat: add lazy loading to image sections
    - Adjust image sections
    - Add lazyload functionality
    - Use smaller images

--- theme/acf-blocks/mobile/image.php
+++ theme/acf-blocks/mobile/image.php
@@ -8,9 +8,12 @@
     <?php } ?>
 
     <div class="section-mobile-inner">
-        <a class="admin-prevent-click image-wrapper section-mobile-image-<?= $args['image']['ID']; ?>" href="<?= $args['link']['url']; ?>">
+        <a class="admin-prevent-click image-wrapper section-mobile-image-<?= $args['image']['ID']; ?>"
+            href="<?= $args['link']['url']; ?>">
             <div class="hover-overlay"><?= $args['link']['title']; ?></div>
-            <img src="<?= $args['image']['sizes']['large']; ?>" title="<?= $args['image']['caption']; ?>">
+            <img data-src="<?= $args['image']['sizes']['large']; ?>"
+                title="<?= $args['image']['caption']; ?>"
+                loading="lazy">
         </a>
     </div>
 
@@ -23,10 +26,19 @@
     <?php } ?>
 
     <div class="section-mobile-inner">
-        <a data-caption="<?= $args['image']['caption']; ?>" class="admin-prevent-click image-wrapper smart-photo section-mobile-image-<?= $args['image']['ID']; ?>" href="<?= $args['image']['url']; ?>" data-group="mobile-group-<?= $args['image']['ID']; ?>">
+        <a data-caption="<?= $args['image']['caption']; ?>"
+            class="admin-prevent-click image-wrapper smart-photo section-mobile-image-<?= $args['image']['ID']; ?>"
+            href="<?= $args['image']['url']; ?>"
+            data-group="mobile-group-<?= $args['image']['ID']; ?>">
             <div class="image-container">
-                <img src="<?= $args['image']['sizes']['large']; ?>" title="<?= $args['image']['caption']; ?>">
-                <div class="caption"><div class="span"><?= $args['image']['caption']; ?></div></div>
+                <img data-src="<?= $args['image']['sizes']['large']; ?>"
+                    title="<?= $args['image']['caption']; ?>"
+                    loading="lazy">
+                <div class="caption">
+                    <div class="span">
+                        <?= $args['image']['caption']; ?>
+                    </div>
+                </div>
             </div>
         </a>
     </div>
