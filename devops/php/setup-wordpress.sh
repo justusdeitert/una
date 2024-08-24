e3fa220a15643a473d5f2996a2d1f4b1d7f39aa4
Justus Deitert <mail@justusdeitert.de>
2024-08-24 08:52:14 +0200

    chore: update WordPress plugins and build tasks
    - Update WordPress plugins
    - Clean up header.php
    - Adjust ACF config
    - Add dev and build tasks to Makefile

--- devops/php/setup-wordpress.sh
+++ devops/php/setup-wordpress.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# wp search-replace 'una-moehrke.de' 'localhost:8080' --allow-root
-
 # Wait for the database to be ready
 /usr/local/bin/wait-for-it.sh mysql:3306 --timeout=30 --strict -- echo "Database is up"
 
@@ -15,27 +13,32 @@ if [ ! -f wp-config.php ]; then
     wp core install --url="$WORDPRESS_URL" --title="$WORDPRESS_TITLE" --admin_user="$WORDPRESS_ADMIN_USER" --admin_password="$WORDPRESS_ADMIN_PASSWORD" --admin_email="$WORDPRESS_ADMIN_EMAIL" --allow-root
 fi
 
-# Define an array of plugin slugs with versions
+# Define the list of plugins to install
 plugins=(
-    "wordpress-seo:19.5"
-    "jetpack:13.7"
-    "clean-image-filenames:1.5"
-    "disable-blog:0.5.4"
-    "enable-media-replace:4.1.5"
-    "imsanity:2.8.2"
-    "disable-comments:2.4.6"
-    "phoenix-media-rename:3.11.8"
-    "svg-support:2.5.8"
-    "user-switching:1.8.0"
-    "pc-robotstxt:1.10"
-    "converter-for-media:5.13.1"
+    "autodescription:5.0.6:activate"
     "cache-enabler:1.8.15"
+    "clean-image-filenames:1.5:activate"
+    "converter-for-media:6.0.0:activate"
+    "disable-blog:0.5.4:activate"
+    "disable-comments:2.4.6:activate"
+    "enable-media-replace:4.1.5:activate"
+    "imsanity:2.8.4:activate"
+    "jetpack:13.7"
+    "phoenix-media-rename:3.11.8:activate"
+    "svg-support:2.5.8:activate"
+    "user-switching:1.8.0:activate"
+    "webp-converter-for-media:6.0.0:activate"
 )
 
 # Install plugins
 for plugin in "${plugins[@]}"; do
-    IFS=":" read -r plugin_slug plugin_version <<< "$plugin"
-    wp plugin install "$plugin_slug" --version="$plugin_version" --activate --allow-root
+    IFS=":" read -r plugin_slug plugin_version plugin_action <<< "$plugin"
+
+    if [ "$plugin_action" == "activate" ]; then
+        wp plugin install "$plugin_slug" --version="$plugin_version" --activate --allow-root
+    else
+        wp plugin install "$plugin_slug" --version="$plugin_version" --allow-root
+    fi
 done
 
 # Start PHP-FPM to keep the container running
