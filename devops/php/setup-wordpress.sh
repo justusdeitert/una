612efa8baaade45777c3d9208cac395728b29e7d
Justus Deitert <mail@justusdeitert.de>
2026-03-03 21:16:39 +0100

    refactor: guard plugin installs and add theme activation
    - move plugin installs inside wp-config.php check to skip on restart
    - activate theme from WORDPRESS_THEME env var
    - use WORDPRESS_VERSION env var for core download

--- devops/php/setup-wordpress.sh
+++ devops/php/setup-wordpress.sh
@@ -12,36 +12,39 @@ fi
 
 if [ ! -f wp-config.php ]; then
     echo "wp core download"
-    wp core download --version=6.9.4 --allow-root
+    wp core download --version="$WORDPRESS_VERSION" --allow-root
 
     echo "wp config create"
     wp config create --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" --dbhost="$MYSQL_HOST" --dbpass="$MYSQL_PASSWORD" --allow-root
 
     echo "wp core install"
     wp core install --url="$WORDPRESS_URL" --title="$WORDPRESS_TITLE" --admin_user="$WORDPRESS_ADMIN_USER" --admin_password="$WORDPRESS_ADMIN_PASSWORD" --admin_email="$WORDPRESS_ADMIN_EMAIL" --allow-root
+
+    plugins=(
+        "autodescription:5.1.4:activate"
+        "cache-enabler:1.8.16"
+        "clean-image-filenames:1.5:activate"
+        "disable-blog:0.5.5:activate"
+        "disable-comments:2.7.0:activate"
+        "enable-media-replace:4.1.8:activate"
+        "imsanity:2.9.0:activate"
+        "jetpack:15.7"
+        "phoenix-media-rename:3.13.1:activate"
+        "svg-support:2.5.14:activate"
+        "user-switching:1.11.2:activate"
+        "webp-converter-for-media:6.5.5:activate"
+    )
+
+    for plugin in "${plugins[@]}"; do
+        IFS=":" read -r plugin_slug plugin_version plugin_action <<< "$plugin"
+
+        if [ "$plugin_action" == "activate" ]; then
+            wp plugin install "$plugin_slug" --version="$plugin_version" --activate --allow-root
+        else
+            wp plugin install "$plugin_slug" --version="$plugin_version" --allow-root
+        fi
+    done
+
+    echo "Activating theme $WORDPRESS_THEME"
+    wp theme activate "$WORDPRESS_THEME" --allow-root
 fi
-
-plugins=(
-    "autodescription:5.1.4:activate"
-    "cache-enabler:1.8.16"
-    "clean-image-filenames:1.5:activate"
-    "disable-blog:0.5.5:activate"
-    "disable-comments:2.7.0:activate"
-    "enable-media-replace:4.1.8:activate"
-    "imsanity:2.9.0:activate"
-    "jetpack:15.7"
-    "phoenix-media-rename:3.13.1:activate"
-    "svg-support:2.5.14:activate"
-    "user-switching:1.11.2:activate"
-    "webp-converter-for-media:6.5.5:activate"
-)
-
-for plugin in "${plugins[@]}"; do
-    IFS=":" read -r plugin_slug plugin_version plugin_action <<< "$plugin"
-
-    if [ "$plugin_action" == "activate" ]; then
-        wp plugin install "$plugin_slug" --version="$plugin_version" --activate --allow-root
-    else
-        wp plugin install "$plugin_slug" --version="$plugin_version" --allow-root
-    fi
-done
