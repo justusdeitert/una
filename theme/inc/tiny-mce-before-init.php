015e5c886b9e3aff215f347c6ef7311e86ca4361
Justus Deitert <mail@justusdeitert.de>
2026-04-19 08:27:28 +0000

    fix: sanitize WYSIWYG content on save
    - Strip &nbsp;, &thinsp; and their Unicode equivalents from WYSIWYG fields
    - Remove empty inline tags wrapping only whitespace (e.g. <i> </i>)
    - Collapse multiple consecutive spaces into one
    - Add git push confirmation rule to copilot instructions

--- theme/inc/tiny-mce-before-init.php
+++ theme/inc/tiny-mce-before-init.php
@@ -28,3 +28,16 @@ add_filter('tiny_mce_before_init', function ($mce_init) {
 
     return $mce_init;
 });
+
+// Strip non-breaking spaces and empty inline tags from WYSIWYG fields on save
+add_filter('acf/update_value/type=wysiwyg', function ($value) {
+    if (is_string($value)) {
+        $value = str_replace(["\xC2\xA0", '&nbsp;', '&thinsp;', "\xE2\x80\x89"], ' ', $value);
+        // Remove inline tags that only contain whitespace (e.g. <i> </i>, <b> </b>)
+        $value = preg_replace('/<(i|b|em|strong|span)>\s*<\/\1>/', ' ', $value);
+        // Collapse multiple consecutive spaces into one
+        $value = preg_replace('/  +/', ' ', $value);
+    }
+
+    return $value;
+});
