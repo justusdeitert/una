8e50cdb22cb9f04841444b2012e224d403a2b3fd
Justus Deitert <mail@justusdeitert.de>
2026-04-22 17:55:42 +0000

    feat: improve text block editor controls and WYSIWYG toolbar
    - add width select (narrow/default/wide col) to text layout
    - add container padding select (7% default / 12% narrow) to text layout
    - add content-container-narrow SCSS modifier for desktop
    - add styleselect button to WYSIWYG toolbar row 2
    - rename dark style to medium text (22px), add big text style (28px)
    - add h1 and h2 to block format dropdown
    - add descriptions to all text layout SCF fields
    - fix empty id attribute and extra class spaces on text-container
    - remove redundant :first-child padding-top in .section-mobile
    - fix padding shorthand on .content-container (cosmetic)

--- theme/acf-blocks/desktop/text.php
+++ theme/acf-blocks/desktop/text.php
@@ -2,21 +2,40 @@
 $accordion = get_sub_field('accordion');
 $text = get_sub_field('text');
 $full_width = get_sub_field('full_width');
-$accordion_class = '';
+$width = get_sub_field('width') ?: 'default';
+$container_padding = get_sub_field('container_padding') ?: 'default';
 $accordion_id = '';
 $accordion_lines = 8;
+
+$width_class_map = [
+    'default' => 'col-md-10',
+    'wide' => 'col-md-12',
+    'narrow' => 'col-md-8',
+];
+$column_class = $width_class_map[$width] ?? $width_class_map['default'];
+
+$content_container_classes = ['content-container', 'text-container-accordion'];
+if ($container_padding === 'narrow') {
+    $content_container_classes[] = 'content-container-narrow';
+}
+
+$text_container_classes = ['text-container'];
+
+if ($accordion) {
+    $accordion_id = get_sub_field_object('accordion')['key'];
+    $accordion_lines = get_sub_field('accordion_lines') ?: 8;
+    $text_container_classes[] = $accordion_id;
+    $text_container_classes[] = 'collapse';
+}
+
+if ($full_width) {
+    $text_container_classes[] = 'full-width';
+}
 ?>
-<div class="content-container text-container-accordion">
-    <?php if ($accordion) { ?>
-        <?php
-            $accordion_class = 'collapse';
-        $accordion_id = get_sub_field_object('accordion')['key'];
-        $accordion_lines = get_sub_field('accordion_lines') ?: 8;
-        ?>
-    <?php } ?>
-    <div id="<?= $accordion_id; ?>" class="text-container <?= $accordion_id; ?> <?= $accordion_class; ?> <?php if ($full_width) { ?>full-width<?php } ?>" <?php if ($accordion) { ?>data-collapse-lines="<?= intval($accordion_lines); ?>" style="height: <?= round((intval($accordion_lines) + 0.5) * 16 * 1.4); ?>px"<?php } ?>>
+<div class="<?= esc_attr(implode(' ', $content_container_classes)); ?>">
+    <div<?php if ($accordion_id) { ?> id="<?= esc_attr($accordion_id); ?>"<?php } ?> class="<?= esc_attr(implode(' ', $text_container_classes)); ?>"<?php if ($accordion) { ?> data-collapse-lines="<?= intval($accordion_lines); ?>" style="height: <?= round((intval($accordion_lines) + 0.5) * 16 * 1.4); ?>px"<?php } ?>>
         <div class="row">
-            <div class="col-12 col-md-10">
+            <div class="col-12 <?= esc_attr($column_class); ?>">
                 <?php if ($text) { ?>
                     <p><?= wp_kses_post($text); ?></p>
                 <?php } ?>
@@ -25,7 +44,7 @@ $accordion_lines = 8;
     </div>
     <?php if ($accordion) { ?>
         <div class="row">
-            <div class="col-12 col-md-10">
+            <div class="col-12 <?= esc_attr($column_class); ?>">
                 <a role="button" class="accordion-button-bottom collapsed" data-toggle="collapse" href="#<?= $accordion_id; ?>" aria-expanded="false" aria-controls="collapseExample"></a>
             </div>
         </div>
